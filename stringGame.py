705d1022370f5ddf704ec81743c915ff14c93eed
Judson S <judz1105@gmail.com>
2021-10-25 08:07:58 -0700

    drawing lines

--- stringGame.py
+++ stringGame.py
@@ -3,22 +3,28 @@ import pygame
 pygame.init()
 win = pygame.display.set_mode([500,700])
 
-class string:
-    def __init__(x,y,preX,preY):
-        self.x = x
-        self.y = y
-        self.preX = preX
-        self.preY = preY
+previous_point = None
+win.fill((255,255,255))
 
+def draw_mousePos(event):
+    global previous_point
+    click_pos = event.pos
+    if previous_point != None:
+        pygame.draw.line(win, (0,0,0), previous_point, click_pos, 5)
+        previous_point = None
+    previous_point = click_pos
+    
 
 def main():
     run = True
+    previous_point = None
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        win.fill((255,255,255))
-        pygame.display.flip()
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                draw_mousePos(event)
+        pygame.display.update()
     pygame.quit()
 
 
